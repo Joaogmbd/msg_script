784b336e6487ee9a9bed0e03060db2f639c6d2f6
João Gabriel Moreira Bossatto Daré <79703333+Joaogmbd@users.noreply.github.com>
2023-04-11 10:25:39 -0300

    Create send_msg.ps1

---
+++ send_msg.ps1
@@ -0,0 +1,20 @@
+write-host '''
+  _________                  .___                        
+ /   _____/ ____   ____    __| _/   _____   ______ ____  
+ \_____  \_/ __ \ /    \  / __ |   /     \ /  ___// ___\ 
+ /        \  ___/|   |  \/ /_/ |  |  Y Y  \\___ \/ /_/  >
+/_______  /\___  >___|  /\____ |  |__|_|  /____  >___  / 
+        \/     \/     \/      \/        \/     \/_____/  
+''' -foregroundcolor green
+$msg = read-host "Digite a mensagem"
+write-host "Em alguns instantes a mensagem será enviada..."
+$alive=@()
+for ($i=0; $i -le 50){
+	ping -w LAB05-$i -n 1 | Out-Null
+	if ($? -eq $true){
+		$working += "LAB05-$i"
+	}
+} 
+foreach ($i in $working){
+	msg alunodev /server:$i $msg
+}
